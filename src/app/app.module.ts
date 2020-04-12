95e65f8ab02fd2b9b9187f70c9ead49d161b1d8c
muhammeta7 <muhammeta7@gmail.com>
2020-04-11 22:24:52 -0400

    got some minor front-end stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ChannelsComponent } from './channels/channels.component';
+import { MessagesComponent } from './messages/messages.component';
+import { Router, Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+const appRoutes : Routes = [
+  {
+    path: 'channels',
+    component : ChannelsComponent
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent
+  },
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path : '**',
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    NotFoundComponent,
+    ChannelsComponent,
+    MessagesComponent,
+    LoginComponent,
+    FooterComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
